a99a083188be93e1abc427ed9581b7fe866b052f
dyoshino88 <yoshino0707dh@gmail.com>
2023-06-03 08:39:40 +0900

    SECRET_KEY変更

--- bulletinboard/settings.py
+++ bulletinboard/settings.py
@@ -25,7 +25,6 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
