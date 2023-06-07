79f1940b26918ddce471683f43bd4e5d27722ba4
dyoshino88 <yoshino0707dh@gmail.com>
2023-06-07 16:02:12 +0900

    0607

--- bulletinboard/settings.py
+++ bulletinboard/settings.py
@@ -181,7 +181,7 @@ if not DEBUG:
         'whitenoise.middleware.WhiteNoiseMiddleware',
     ]
 
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+    "whitenoise.storage.CompressedStaticFilesStorage"
+    
     # HerokuのConfigを読み込み
     django_heroku.settings(locals())
