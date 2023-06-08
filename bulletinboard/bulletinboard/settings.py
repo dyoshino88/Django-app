73616b01e5cff7b3de1208ad0996b06587148cb7
dyoshino88 <yoshino0707dh@gmail.com>
2023-06-08 15:42:51 +0900

    Fix

--- bulletinboard/bulletinboard/settings.py
+++ bulletinboard/bulletinboard/settings.py
@@ -122,7 +122,6 @@ if IS_HEROKU_APP:
     DATABASES = {
         "default": dj_database_url.config(
             conn_max_age=600,
-            # conn_health_checks=True,
             ssl_require=True,
         ),
     }
