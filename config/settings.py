c292c2f50c1c166a3c95b87158e9a1c2fa88e407
dyoshino88 <yoshino0707dh@gmail.com>
2023-06-13 21:35:49 +0900

    fix0613

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ import os
 import secrets
 from pathlib import Path
 import dj_database_url
-import django_heroku
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -209,5 +209,3 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
