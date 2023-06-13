55f07494e9eae80759c649f40dde77085c47b282
dyoshino88 <yoshino0707dh@gmail.com>
2023-06-13 14:45:39 +0900

    Herokuデプロイのための設定

--- config/settings.py
+++ config/settings.py
@@ -209,3 +209,5 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
