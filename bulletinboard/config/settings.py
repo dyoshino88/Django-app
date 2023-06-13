41932a1b2b0caaca6c423a0b5e1fb4ddd662c7b4
dyoshino88 <yoshino0707dh@gmail.com>
2023-06-13 13:23:31 +0900

    fix

--- bulletinboard/config/settings.py
+++ bulletinboard/config/settings.py
@@ -19,7 +19,7 @@ import django_on_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
