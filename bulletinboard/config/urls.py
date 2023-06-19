8d639ad1c180c39e09a4556aaa8f3b97e49fc6a1
dyoshino88 <yoshino0707dh@gmail.com>
2023-06-19 13:11:48 +0900

    urlchange

--- bulletinboard/config/urls.py
+++ bulletinboard/config/urls.py
@@ -22,6 +22,7 @@ from accounts.views import error_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('board/', include('board.urls')),
 ]
