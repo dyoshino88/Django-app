cc5593a0985c8df908b659e194b2314371154a45
dyoshino88 <yoshino0707dh@gmail.com>
2023-06-25 07:11:32 +0900

    fix 2points

--- bulletinboard/config/settings.py
+++ bulletinboard/config/settings.py
@@ -121,15 +121,12 @@ if IS_HEROKU_APP:
     # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
     # https://github.com/jazzband/dj-database-url
     DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'my_database',
-         'USER': 'yoshinodaisuke',
-         'PASSWORD': 'dy080804',
-         'HOST': 'localhost',
-         'PORT': '5432',
-     }
-}
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
+    }
 else:
     # When running locally in development or in CI, a sqlite database file will be used instead
     # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
