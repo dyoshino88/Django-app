738d59dbabd7d36cbb010bc10f722ac518e06381
dyoshino88 <yoshino0707dh@gmail.com>
2023-07-07 15:42:37 +0900

    board/urls.py change

--- bulletinboard/board/urls.py
+++ bulletinboard/board/urls.py
@@ -6,8 +6,8 @@ app_name = 'board'
 urlpatterns = [
   path('create_topic', views.create_topic, name='create_topic'),
   path('list_topics', views.list_topics, name='list_topics'),
-  path('edit_topic/', views.edit_topic, name='edit_topic'),
-  path('delete_topic/', views.delete_topic, name='delete_topic'),
-  path('post_texts/', views.post_texts, name='post_texts'), 
+  path('edit_topic/<int:id>', views.edit_topic, name='edit_topic'),
+  path('delete_topic/<int:id>', views.delete_topic, name='delete_topic'),
+  path('post_texts/<int:topic_id>', views.post_texts, name='post_texts'), 
   path('save_text', views.save_text, name='save_text'),
 ]
