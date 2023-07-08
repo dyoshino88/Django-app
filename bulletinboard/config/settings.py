07c7393334e1707ebe31f9922888ae44254f229a
dyoshino88 <yoshino0707dh@gmail.com>
2023-07-08 16:07:22 +0900

    settings.py CSRF設定

--- bulletinboard/config/settings.py
+++ bulletinboard/config/settings.py
@@ -213,3 +213,5 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://dkoukan.com']
+
