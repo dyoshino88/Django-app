d81d782bcb7f495c8a1d0cd628494acb89fbaff8
dyoshino88 <yoshino0707dh@gmail.com>
2023-07-25 07:01:43 +0900

    uddate settings.py STORAGES

--- bulletinboard/config/settings.py
+++ bulletinboard/config/settings.py
@@ -194,12 +194,20 @@ STATICFILES_DIRS = [
 ]
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "OPTIONS": {
+            # 任意のオプションを指定できる
+        }
+    },
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
     # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+
 
 # Don't store the original (un-hashed filename) version of static files, to reduce slug size:
 # https://whitenoise.readthedocs.io/en/latest/django.html#WHITENOISE_KEEP_ONLY_HASHED_FILES
