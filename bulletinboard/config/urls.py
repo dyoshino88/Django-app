36d737dd1919f192b5ff8783f0f762c0d2c73e32
dyoshino88 <yoshino0707dh@gmail.com>
2023-08-16 07:05:35 +0900

    404 error

--- bulletinboard/config/urls.py
+++ bulletinboard/config/urls.py
@@ -27,6 +27,5 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path('board/',include('board.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-handler404 = error_page
 
 
